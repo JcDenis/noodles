1efa9066813b026249eace6722904743d1387d3b
Jean-Christian Denis <contact@jcdenis.fr>
2022-12-10 15:43:44 +0100

    use dotclear method to check version and clean config

--- _install.php
+++ _install.php
@@ -14,52 +14,58 @@ if (!defined('DC_CONTEXT_ADMIN')) {
     return null;
 }
 
-try {
-    if (version_compare(
-        dcCore::app()->getVersion('noodles'),
-        dcCore::app()->plugins->moduleInfo('noodles', 'version'),
-        '>='
-    )) {
-        return null;
-    }
-
-    dcCore::app()->blog->settings->addNamespace('noodles');
-    dcCore::app()->blog->settings->noodles->put(
+$mod_conf = [
+    [
         'noodles_active',
         false,
         'boolean',
         'Enable extension',
-        false,
-        true
-    );
-    dcCore::app()->blog->settings->noodles->put(
+    ],
+    [
         'noodles_api',
         'http://www.gravatar.com/',
         'string',
         'external API to use',
-        false,
-        true
-    );
-    dcCore::app()->blog->settings->noodles->put(
+    ],
+    [
         'noodles_image',
         '',
         'string',
         'Image filename',
-        false,
-        true
-    );
-    dcCore::app()->blog->settings->noodles->put(
+    ],
+    [
         'noodles_object',
         '',
         'string',
         'Noodles behaviors',
-        false,
-        true
-    );
+    ],
+];
+
+# -- Nothing to change below --
+try {
+    # Check module version
+    if (!dcCore::app()->newVersion(
+        basename(__DIR__), 
+        dcCore::app()->plugins->moduleInfo(basename(__DIR__), 'version')
+    )) {
+        return null;
+    }
+    # Set module settings
+    dcCore::app()->blog->settings->addNamespace(basename(__DIR__));
+    foreach ($mod_conf as $v) {
+        dcCore::app()->blog->settings->__get(basename(__DIR__))->put(
+            $v[0],
+            $v[1],
+            $v[2],
+            $v[3],
+            false,
+            true
+        );
+    }
 
     return true;
 } catch (Exception $e) {
     dcCore::app()->error->add($e->getMessage());
-}
 
-return false;
+    return false;
+}
