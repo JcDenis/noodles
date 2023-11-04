acb524a48d470a7d997bc7e150685a227b1f0724
Jean-Christian Denis <contact@jcdenis.fr>
2023-11-04 18:58:41 +0100

    fix perms

--- src/My.php
+++ src/My.php
@@ -26,9 +26,8 @@ class My extends MyPlugin
     protected static function checkCustomContext(int $context): ?bool
     {
         return match ($context) {
-            // Allow BACKEND and MANAGE and MENU to also content admin
-            self::BACKEND, self::MANAGE, self::MENU => App::task()->checkContext('BACKEND')
-                && App::blog()->isDefined()
+            // Add content admin perm to backend
+            self::MANAGE, self::MENU => App::task()->checkContext('BACKEND')
                 && App::auth()->check(App::auth()->makePermissions([
                     App::auth()::PERMISSION_CONTENT_ADMIN,
                 ]), App::blog()->id()),
